b5fee32a39f537d7a0d8b0e77104e2dd29b8b68a
cambriahu <136650729+cambriahu@users.noreply.github.com>
2023-09-13 16:25:08 -0500

    Add files via upload

---
+++ BalancedBrackets.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedBracketsNS
+{
+    
+   
+        public static class BalancedBrackets
+        {
+            public static bool HasBalancedBrackets(string input)
+            {
+                if (string.IsNullOrEmpty(input))
+                {
+                    return true; // An empty string is considered balanced.
+                }
+
+                Stack<Char> stack = new Stack<Char>();
+
+                foreach (Char character in input)
+                {
+                    if (character == '[')
+                    {
+                        stack.Push(character);
+                    }
+                    else if (character == ']')
+                    {
+                        if (stack.Count == 0 || stack.Pop() != '[')
+                        {
+                            return false; // Mismatched or unmatched closing bracket
+                        }
+                    }
+                    // Ignore non-bracket characters
+                }
+
+                return stack.Count == 0; // All brackets are matched.
+            }
+        }
+    }
